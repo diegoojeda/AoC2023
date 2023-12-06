4ec97893a692566bb9de80ea26f8d935d2d79d97
Diego <diego.ojeda@apiumhub.com>
2023-12-06 16:04:57 +0100

    Add time profiling

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -2,19 +2,19 @@ package day06
 
 import println
 import readInput
+import kotlin.system.measureTimeMillis
 
 data class Race(val time: Long, val distance: Long) {
     private fun calculateDistance(speed: Long, time: Long): Long = speed * time
 
-    fun calculateResults(): Long = (0..time).map { calculateDistance(it, time - it) }.count { it > distance }.toLong()
+    fun calculateResults(): Long = (0..time).count { calculateDistance(it, time - it) > distance }.toLong()
 }
 
 fun main() {
     fun parseLinePart1(line: String) =
         line.split(":").last().split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toLong() }
 
-    fun parseLinePart2(line: String) =
-        line.split(":").last().replace(" ", "").trim().toLong()
+    fun parseLinePart2(line: String) = line.split(":").last().replace(" ", "").trim().toLong()
 
     fun parseRaces(input: List<String>): List<Race> {
         val allTimes = input.first()
@@ -24,22 +24,34 @@ fun main() {
         return times.mapIndexed { index, s -> Race(s, distances[index]) }
     }
 
-    fun part1(input: List<String>): Long = parseRaces(input).fold(1) { acc, next -> acc * next.calculateResults() }
+    fun part1(input: List<String>): Long {
+        var result: Long
+        val timeMilis = measureTimeMillis {
+            result = parseRaces(input).fold(1) { acc, next -> acc * next.calculateResults() }
+        }
+        println("Time Millis part 1: $timeMilis")
+        return result
+    }
 
     fun part2(input: List<String>): Long {
-        val allTimes = input.first()
-        val allDistances = input.last()
-        val time = parseLinePart2(allTimes)
-        val distance = parseLinePart2(allDistances)
-        val singleRace = Race(time, distance)
-        return singleRace.calculateResults();
+        var result: Long
+        val timeMilis = measureTimeMillis {
+            val allTimes = input.first()
+            val allDistances = input.last()
+            val time = parseLinePart2(allTimes)
+            val distance = parseLinePart2(allDistances)
+            val singleRace = Race(time, distance)
+            result = singleRace.calculateResults();
+        }
+        println("Time Millis part 2: $timeMilis")
+        return result
     }
 
     val testInputPart1 = readInput("day06/Day06_test_part1")
     val inputPart1 = readInput("day06/Day06_part_1")
 
     check(part1(testInputPart1) == 288L)
-    part1(inputPart1).println()
     check(part2(testInputPart1) == 71503L)
+    part1(inputPart1).println()
     part2(inputPart1).println()
 }
